6af446fd9d09aef8bf4ad85f686e89dfd5ae511c
valen <kirksilvia9@gmail.com>
2026-04-30 11:57:47 -0300

    primer commit

---
+++ src/test/java/veterinaria_tp_jpa/demo2/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package veterinaria_tp_jpa.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
